4ecf7bf4801010f51d4726de0258d82e61da1d76
luckylighter <vknyaz06@gmail.com>
2022-08-07 15:30:33 -0400

    3rd upload

---
+++ src/Player.java
@@ -0,0 +1,7 @@
+public interface Player {
+    String getName();
+    int getAttackStrength();
+    int getDefenceStrength();
+
+
+}
---
+++ src/PlayerImpl.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerImpl implements Player{
+
+    private String name;
+    private int attackStrength;
+    private int defenceStrength;
+    private Armor headArmor;
+    private ArrayList<Armor> handArmor;
+    private ArrayList<Armor> footArmor;
+
+
+    // initialize player to have a name and no gear and no attack or defence strength
+    public PlayerImpl(String name) {
+        this.name = name;
+        this.attackStrength = 0;
+        this.defenceStrength = 0;
+        this.headArmor = null;
+        this.handArmor = new ArrayList<>();
+        this.footArmor = new ArrayList<>();
+    }
+
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAttackStrength() {
+        this.attackStrength = this.attackStrength + this.headArmor.getAttackStrength();
+
+        for (Armor armor : this.handArmor) {
+            this.attackStrength += armor.getAttackStrength();
+        }
+        for (Armor armor : this.footArmor) {
+            this.attackStrength += armor.getAttackStrength();
+        }
+        return this.attackStrength;
+    }
+
+    @Override
+    public int getDefenceStrength() {
+        this.defenceStrength = this.defenceStrength + this.headArmor.getDefenceStrength();
+
+        for (Armor armor : this.handArmor) {
+            this.defenceStrength += armor.getDefenceStrength();
+        }
+        for (Armor armor : this.footArmor) {
+            this.defenceStrength += armor.getDefenceStrength();
+        }
+        return this.defenceStrength;
+    }
+
+
+}
