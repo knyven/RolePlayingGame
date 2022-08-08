5f82c16b2592d3c91c940364f51e944fa1c8c6b6
sahsahil1998 <sahsahil1997@gmail.com>
2022-08-07 22:46:09 -0400

    fight

--- src/FightImpl.java
+++ src/FightImpl.java
@@ -1,8 +1,22 @@
 import java.util.ArrayList;
+import java.util.List
 
 public class FightImpl {
 
     private Player player1;
     private Player player2;
+    private List<Armor> armors = new ArrayList<>();
+    private int armorTotal;
 
+    public FightImpl(Player player1, Player player2, List<Armor> armors){
+
+        //if(player1.getSize())
+
+        if (armors.size() != 10){
+            throw new IllegalArgumentException("Total pieces of armor must amount to" + armorTotal);
+
+        }
+
+
+    }
 }
